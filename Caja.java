e2d43f0b40371d330d6c11990b9b3045f1503cd1
203719 <203719@ids.upchiapas.edu.mx>
2021-02-08 07:26:42 -0600

    Primer version

---
+++ Caja.java
@@ -0,0 +1,18 @@
+public class Caja {
+    private String nombre;
+    private int peso;  
+
+    public Caja(String nombre,int peso) {
+       this.nombre = nombre;
+       this.peso = peso;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getPeso(){
+        return peso;
+    }
+
+
+}
